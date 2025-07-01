ba6fa2619957e61ecb88968c8e2db5ff6488e8dc
Lucas <lucas.taladrid@hotmail.es>
2025-07-01 12:43:15 +0200

    Ejercicio de encontrar el número que se repite de forma impar

---
+++ src/encontrarNImpar/EncontrarNImpar.java
@@ -0,0 +1,32 @@
+package encontrarNImpar;
+
+public class EncontrarNImpar {
+    public static int findIt(int[] a) {
+        int count=0;
+        for(Integer n: a){
+            for(Integer k: a){
+                if(n==k){
+                    count++;
+                }
+            }
+            if(count%2!=0){
+                return n;
+            }else{
+                count=0;
+            }
+        }
+       return -1;
+    }
+/*
+La forma más eficiente podría ser esta
+El operador ^ irá añadiendo o sumando los número según aparezcan por ejemplo 1^1=0 pero 1^1^1=1, el resultado final será el número que se ha repetido de forma impar
+public static int findIt(int[] A) {
+  	int xor = 0;
+    for (int i = 0; i < A.length; i++) {
+    	xor ^= A[i];
+    }
+    return xor;
+  }
+ */
+
+}
